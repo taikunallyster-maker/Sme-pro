cdb9f33e76922c4b5d75a942f5bd2e01c996f157
taikunallyster-maker <taikunallyster@gmail.com>
2026-05-23 16:27:16 +0300

    Create i18n.js

---
+++ src/i18n.js
@@ -0,0 +1,18 @@
+export const translations = {
+  en: {
+    sales: "Sales",
+    expenses: "Expenses",
+    profit: "Profit",
+    addSale: "Add Sale",
+    addExpense: "Add Expense",
+    invoice: "Generate Invoice"
+  },
+  sw: {
+    sales: "Mauzo",
+    expenses: "Matumizi",
+    profit: "Faida",
+    addSale: "Ongeza Mauzo",
+    addExpense: "Ongeza Matumizi",
+    invoice: "Tengeneza Ankara"
+  }
+};
